91e0ed72d6a7386e589326add0c9fded5b5dfe23
peperium <tomdiddleston2022@gmail.com>
2023-09-23 05:40:18 +0700

    Json type edit

--- runtime/types/types.go
+++ runtime/types/types.go
@@ -4,9 +4,12 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
+	"log"
 	"strconv"
 	"time"
 
+	"github.com/99designs/gqlgen/graphql"
 	"github.com/shopspring/decimal"
 )
 
@@ -54,25 +57,30 @@ func (m *BigInt) MarshalJSON() ([]byte, error) {
 }
 
 // JSON is a new type which implements the correct internal prisma (un)marshaller
-type JSON json.RawMessage
+type JSON map[string]interface{}
 
-// MarshalJSON returns m as the JSON encoding of m.
-func (m JSON) MarshalJSON() ([]byte, error) {
-	if m == nil {
-		return []byte("null"), nil
-	}
-	return []byte(fmt.Sprintf("%q", m)), nil
+func MarshalJSON(b JSON) graphql.Marshaler {
+	return graphql.WriterFunc(func(w io.Writer) {
+		byteData, err := json.Marshal(b)
+		if err != nil {
+			log.Printf("FAIL WHILE MARSHAL JSON %v\n", string(byteData))
+		}
+		_, err = w.Write(byteData)
+		if err != nil {
+			log.Printf("FAIL WHILE WRITE DATA %v\n", string(byteData))
+		}
+	})
 }
 
-// UnmarshalJSON sets *m to a copy of data.
-func (m *JSON) UnmarshalJSON(data []byte) error {
-	if m == nil {
-		return errors.New("JSON: UnmarshalJSON on nil pointer")
-	}
-	str, err := strconv.Unquote(string(data))
+func UnmarshalJSON(v interface{}) (JSON, error) {
+	byteData, err := json.Marshal(v)
 	if err != nil {
-		return fmt.Errorf("JSON: UnmarshalJSON error: %w", err)
+		return JSON{}, fmt.Errorf("FAIL WHILE MARSHAL SCHEME")
 	}
-	*m = append((*m)[0:0], str...)
-	return nil
+	tmp := make(map[string]interface{})
+	err = json.Unmarshal(byteData, &tmp)
+	if err != nil {
+		return JSON{}, fmt.Errorf("FAIL WHILE UNMARSHAL SCHEME")
+	}
+	return tmp, nil
 }
